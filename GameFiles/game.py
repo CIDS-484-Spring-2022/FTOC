b664c3cc3d692d134f39dc21bd1a91c102d930dc
DESKTOP-9QTB75Q\coltt <colttrev@hotmail.com>
2022-04-21 15:51:59 -0500

    Connected the game loop and menu

--- GameFiles/game.py
+++ GameFiles/game.py
@@ -2,6 +2,22 @@ from re import S
 import pygame
 import Settings
 from menu import *
+import Level_Data
+from Level import Level
+from Player import Player
+
+mAdventurerPath = ""
+BG_IMAGE = pygame.image.load('GameFiles/Assets/GreenHillsBG.png')
+BG_IMAGE = pygame.transform.scale(BG_IMAGE, (Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT))
+
+tile_size = 50
+
+WIN = pygame.display.set_mode((Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT))
+pygame.display.set_caption("Freddy Takes on College!")
+
+
+
+
 
 class Game():
     def __init__(self):
@@ -11,23 +27,53 @@ class Game():
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
         self.display = pygame.Surface((Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT))
         self.window = pygame.display.set_mode(((Settings.WINDOW_WIDTH,Settings.WINDOW_HEIGHT)))
-        self.font_name = pygame.font.get_default_font()
+        self.font_name = pygame.font.Font("GameFiles/Assets/ARCADECLASSIC.TTF", 16)
         self.WHITE, self.BLACK, self.RED = (255, 255, 255), (0, 0, 0), (255, 0, 0)
         self.main_menu = MainMenu(self)
         self.options = ControlsMenu(self)
+        self.levels = LevelSelection(self)
         self.curr_menu = self.main_menu
 
+    def draw_grid():
+        for line in range(0, (int(Settings.WINDOW_HEIGHT/tile_size))):
+            pygame.draw.line(WIN, (255, 255, 255), (0, line * tile_size), (Settings.WINDOW_WIDTH, line * tile_size))
+        for line in range(0, (int(Settings.WINDOW_WIDTH/tile_size))):
+            pygame.draw.line(WIN, (255, 255, 255), (line * tile_size, 0), (line * tile_size, Settings.WINDOW_HEIGHT))
+
     def game_loop(self):
+
+        # GUI/Game code to go here
+        clock = pygame.time.Clock()
+        run = True
+    
+        player = Player(100, Settings.WINDOW_HEIGHT-130, 5, 'GameFiles/Assets/character_maleAdventurer_idle.png', 'maleAdventurer')
+        level_1 = Level(Level_Data.level1, tile_size, WIN)
+
+        while run:
+            clock.tick(Settings.FPS)
+
+            WIN.blit(BG_IMAGE, (0, 0))
+            #draw_grid()
+            level_1.draw_level()
+            player.update(WIN, level_1) # hard-coded for now
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                
+
+            pygame.display.update()
+
+        pygame.quit()
+
+    """
         while self.playing:
             self.check_events()
-            if self.START_KEY:
-                self.playing = False
-            self.display.fill(self.BLACK)
-            self.draw_text('Level 1', 20, Settings.WINDOW_WIDTH/2, Settings.WINDOW_HEIGHT/2)
             #this is where you would add our game levels
             self.window.blit(self.display, (0,0))
             pygame.display.update()
             self.reset_keys()
+    """
 
     def check_events(self):
         for event in pygame.event.get():
@@ -48,8 +94,8 @@ class Game():
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False
 
     def draw_text(self, text, size, x, y):
-        font = pygame.font.Font(self.font_name, size)
-        text_surface = font.render(text, True, self.RED)
+        font = pygame.font.Font("GameFiles/Assets/ARCADECLASSIC.TTF", size)
+        text_surface = font.render(text, True, self.WHITE)
         text_rect = text_surface.get_rect()
         text_rect.center = (x,y)
         self.display.blit(text_surface, text_rect)
--- GameFiles/menu.py
+++ GameFiles/menu.py
@@ -20,10 +20,10 @@ class Menu():
 class MainMenu(Menu):
     def __init__(self, game):
         Menu.__init__(self, game)
-        self.state = "Start"
-        self.startx, self.starty = self.mid_w, self.mid_h + 30
+        self.state = "Levels"
+        self.levelsx, self.levelsy = self.mid_w, self.mid_h + 30
         self.optionsx, self.optionsy = self.mid_w, self.mid_h + 50
-        self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
+        self.cursor_rect.midtop = (self.levelsx + self.offset, self.levelsy)
         pygame.display.set_caption("FTOC")
 
 
@@ -33,8 +33,8 @@ class MainMenu(Menu):
             self.game.check_events()
             self.check_input()
             self.game.display.fill(self.game.BLACK)
-            self.game.draw_text('Freddy Takes on College!', 20, Settings.WINDOW_WIDTH / 2, Settings.WINDOW_HEIGHT / 2 - 20)
-            self.game.draw_text("Level Selection", 20, self.startx, self.starty)
+            self.game.draw_text('Freddy Takes on College!', 50, Settings.WINDOW_WIDTH / 2, Settings.WINDOW_HEIGHT / 2 - 20)
+            self.game.draw_text("Level Selection", 20, self.levelsx, self.levelsy)
             self.game.draw_text("Controls", 20, self.optionsx, self.optionsy)
             self.draw_cursor()
             self.blit_screen()
@@ -42,19 +42,19 @@ class MainMenu(Menu):
 
     def move_cursor(self):
         if self.game.DOWN_KEY:
-            if self.state == 'Start':
+            if self.state == 'Levels':
                 self.cursor_rect.midtop = (self.optionsx + self.offset, self.optionsy)
                 self.state = 'Controls'
             elif self.state == 'Controls':
-                self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
-                self.state = 'Start'
+                self.cursor_rect.midtop = (self.levelsx + self.offset, self.levelsy)
+                self.state = 'Levels'
         elif self.game.UP_KEY:
-            if self.state == 'Start':
+            if self.state == 'Levels':
                 self.cursor_rect.midtop = (self.optionsx + self.offset, self.optionsy)
-                self.state = 'Options'
+                self.state = 'Controls'
             elif self.state == 'Controls':
-                self.cursor_rect.midtop = (self.startx + self.offset, self.starty)
-                self.state = 'Start'
+                self.cursor_rect.midtop = (self.levelsx + self.offset, self.levelsy)
+                self.state = 'Levels'
 
     def check_input(self):
         self.move_cursor()
@@ -63,6 +63,8 @@ class MainMenu(Menu):
                 self.game.playing = True
             elif self.state == 'Controls':
                 self.game.curr_menu = self.game.options
+            elif self.state == "Levels":
+                self.game.curr_menu = self.game.levels
             self.run_display = False
 
 class ControlsMenu(Menu):
@@ -75,8 +77,16 @@ class ControlsMenu(Menu):
         while self.run_display:
             self.game.check_events()
             self.check_input()
-            self.game.display.fill((0, 0, 0))
-            self.game.draw_text('Game Controls:', 20, Settings.WINDOW_WIDTH / 2, Settings.WINDOW_HEIGHT / 2 - 30)
+            self.game.display.fill(self.game.BLACK)
+            self.game.draw_text('Game Controls', 40, Settings.WINDOW_WIDTH / 2, Settings.WINDOW_HEIGHT / 2 - 60)
+            self.game.draw_text('Menu', 30, Settings.WINDOW_WIDTH / 2, Settings.WINDOW_HEIGHT / 2-30)
+            self.game.draw_text('Backspace = Previous Menu', 12, Settings.WINDOW_WIDTH / 2, Settings.WINDOW_HEIGHT / 2-15)
+            self.game.draw_text('Enter = Select', 12, Settings.WINDOW_WIDTH / 2, Settings.WINDOW_HEIGHT / 2)
+            self.game.draw_text('Up Arrow = Move Up', 12, Settings.WINDOW_WIDTH / 2, Settings.WINDOW_HEIGHT / 2+15)
+            self.game.draw_text('Down Arrow = Move Down', 12, Settings.WINDOW_WIDTH / 2, Settings.WINDOW_HEIGHT / 2+30)
+            self.game.draw_text('Game', 30, Settings.WINDOW_WIDTH / 2, Settings.WINDOW_HEIGHT / 2+45)
+
+
             self.draw_cursor()
             self.blit_screen()
 
@@ -84,3 +94,76 @@ class ControlsMenu(Menu):
         if self.game.BACK_KEY:
             self.game.curr_menu = self.game.main_menu
             self.run_display = False
+
+
+class LevelSelection(Menu):
+    def __init__(self, game):
+        Menu.__init__(self, game)
+        self.state= "Level1"
+        self.level1x, self.level1y = self.mid_w, self.mid_h + 30
+        self.start = False
+        self.level2x, self.level2y = self.mid_w, self.mid_h + 50
+        self.level3x, self.level3y = self.mid_w, self.mid_h + 70
+        self.level4x, self.level4y = self.mid_w, self.mid_h + 90
+        self.cursor_rect.midtop = (self.level1x + self.offset, self.level1y)
+
+    def check_input(self):
+        self.move_cursor()
+        if self.game.BACK_KEY:
+            self.game.curr_menu = self.game.main_menu
+            self.run_display = False
+        elif self.game.START_KEY:
+            if self.state == "Level1":
+                self.game.playing = True
+
+    def display_menu(self):
+        self.run_display = True
+        while self.run_display:
+            self.game.check_events()
+            self.check_input()
+            self.game.display.fill(self.game.BLACK)
+
+            self.game.draw_text('Level Selection', 30, Settings.WINDOW_WIDTH/2, Settings.WINDOW_HEIGHT/3)
+
+            self.game.draw_text('Level 1', 20, Settings.WINDOW_WIDTH/2, Settings.WINDOW_HEIGHT/2+30)
+            self.game.draw_text('Level 2', 20, Settings.WINDOW_WIDTH/2, Settings.WINDOW_HEIGHT/2+50)
+            self.game.draw_text('Level 3', 20, Settings.WINDOW_WIDTH/2, Settings.WINDOW_HEIGHT/2+70)
+            self.game.draw_text('Level 4', 20, Settings.WINDOW_WIDTH/2, Settings.WINDOW_HEIGHT/2+90)
+            self.draw_cursor()
+            self.blit_screen()
+            #this is where you would add our game levels
+
+
+    def move_cursor(self):
+        if self.game.DOWN_KEY:
+            if self.state == 'Level1':
+                self.cursor_rect.midtop = (self.level2x + self.offset, self.level2y)
+                self.state = 'Level2'
+            elif self.state == 'Level2':
+                self.cursor_rect.midtop = (self.level3x + self.offset, self.level3y)
+                self.state = 'Level3'
+            elif self.state == 'Level3':
+                self.cursor_rect.midtop = (self.level4x + self.offset, self.level4y)
+                self.state = 'Level4'
+            elif self.state == 'Level4':
+                self.cursor_rect.midtop = (self.level1x + self.offset, self.level1y)
+                self.state = 'Level1'
+        elif self.game.UP_KEY:
+            if self.state == 'Level1':
+                self.cursor_rect.midtop = (self.level4x + self.offset, self.level4y)
+                self.state = 'Level4'
+            elif self.state == 'Level2':
+                self.cursor_rect.midtop = (self.level3x + self.offset, self.level3y)
+                self.state = 'Level3'
+            elif self.state == 'Level3':
+                self.cursor_rect.midtop = (self.level2x + self.offset, self.level2y)
+                self.state = 'Level4'
+            elif self.state == 'Level4':
+                self.cursor_rect.midtop = (self.level1x + self.offset, self.level1y)
+                self.state = 'Level1'
+        elif self.game.START_KEY:
+            if self.state == 'Level1':
+                self.run_display = False
+                self.start = True
+
+
